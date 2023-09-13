7d9d3e05e4b1dd914fc1fed4e6587dc52d292b44
Juampibruera <136599420+Juampibruera@users.noreply.github.com>
2023-09-12 23:53:13 -0300

    Update exercise2.py

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -8,10 +8,11 @@ True si al menos una de las variables es True.
 
 esta_lloviendo = True
 riego_activado = True
+piso_mojado = False
 
-# COMPLETAR - INICIO
+if esta_lloviendo or riego_activado == True:
+    piso_mojado = True
 
-# COMPLETAR - FIN
 
 assert piso_mojado
 
@@ -24,10 +25,13 @@ Restricción: Usar NOT.
 
 lado_cuadrado = 5
 area_cuadrado = pow(lado_cuadrado, 2)
+area_mayor_a_cinco = False
 
-# COMPLETAR - INICIO
+if area_cuadrado > 5:
+    print(not area_mayor_a_cinco)
+else:
+    print(area_mayor_a_cinco)
 
-# COMPLETAR - FIN
 
 assert area_mayor_a_cinco
 
@@ -39,10 +43,12 @@ True si el número 1 es divisible por 7 y al mismo tiempo el número 2 no lo es.
 
 numero_1 = 49
 numero_2 = 50
+resultado = True
 
-# COMPLETAR - INICIO
-
-# COMPLETAR - FIN
+if numero_1 % 7 == 0 and numero_2 % 7 != 0:
+    print(resultado)
+else:
+    print(not resultado)
 
 assert resultado
 
@@ -59,8 +65,10 @@ variable_03 = 80
 variable_04 = "90"
 variable_05 = 100
 
-# COMPLETAR - INICIO
-
-# COMPLETAR - FIN
+resultado = (
+    (not variable_01 or variable_02)
+    or (not variable_03 or variable_04)
+    or (not variable_05 or variable_01)
+)
 
 assert resultado == 80
